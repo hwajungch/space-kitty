10e07feebc9ebcf96e2979c751b9fde0f9e1a595
hjchoi28 <63829653+hjchoi28@users.noreply.github.com>
2023-02-18 23:54:18 -0800

    Updates

--- index.js
+++ index.js
@@ -6,25 +6,32 @@ function preload() {
 
 function setup() {
   new Canvas(windowWidth, windowHeight);
-  kitty = createSprite(width/2, height/2);
-  kitty.addImage(kittyImg);
-
-  asteroids = new Group();
-  for (let i = 0; i < 10; i++) {
-    let a = createSprite(random(width), random(height), 25, 25);
-    a.shapeColor = color(random(200, 255));
-    asteroids.add(a);
-  }
+  setupKitty();
+  setupAsteroids();
 }
 
 function draw() {
   background(color(11, 11, 70)); // fixes kitty image trail
 
-  fill(0);
+  fill(255); //white
   textSize(24);
   textAlign(CENTER);
   text('Avoid the asteroids!', width / 2, height / 2);
 
   kitty.position.x = mouseX;
   kitty.position.y = mouseY;
+}
+
+function setupKitty() {
+  kitty = createSprite(width/2, height/2); //spawns in center
+  kitty.addImage(kittyImg);
+}
+
+function setupAsteroids() {
+  asteroids = new Group();
+  for (let i = 0; i < 10; i++) {
+    let a = createSprite(random(width), random(height), 25, 25);
+    a.shapeColor = color(random(100, 255));
+    asteroids.add(a);
+  }
 }
