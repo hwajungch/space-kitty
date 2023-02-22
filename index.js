78807cbc194b4031c8f3288337cf872c26e94a92
hjchoi28 <63829653+hjchoi28@users.noreply.github.com>
2023-02-21 23:01:38 -0800

    game complete

--- index.js
+++ index.js
@@ -1,30 +1,73 @@
-let kitty, kittyImg;
-let asteroids;
+let kitty, kittyImg, asteroids, asteroidImg, survivalTime, tutorialOver;
+
 function preload() {
   kittyImg = loadImage('kitty.png');
+  asteroidImg = loadImage('https://cdn-icons-png.flaticon.com/64/7480/7480279.png');
+  kitty = new Sprite(width/2, height/2); //spawns in center
+  kitty.addImage(kittyImg);
+  asteroids = new Group();
+  asteroids.addImage(asteroidImg);
 }
 
 function setup() {
   new Canvas(windowWidth, windowHeight);
-  kitty = createSprite(width/2, height/2);
-  kitty.addImage(kittyImg);
-
-  asteroids = new Group();
-  for (let i = 0; i < 10; i++) {
-    let a = createSprite(random(width), random(height), 25, 25);
-    a.shapeColor = color(random(200, 255));
-    asteroids.add(a);
-  }
+  survivalTime = 0;
+  setInterval(timer,1000);
+  createAsteroids();
 }
 
 function draw() {
   background(color(11, 11, 70)); // fixes kitty image trail
 
-  fill(0);
+  fill(255); //white
   textSize(24);
   textAlign(CENTER);
-  text('Avoid the asteroids!', width / 2, height / 2);
+  if (tutorialOver) {
+    text('Help space kitty! You can do it!', width / 2, height / 16);
+  } else {
+    text('Move your mouse to avoid the asteroids!', width / 2, height / 16);
+  }
 
   kitty.position.x = mouseX;
   kitty.position.y = mouseY;
+
+  text('Time: ' + survivalTime + ' seconds', width / 2, height*2 / 16);
+
+  // from p5play.org demos
+  for (let a of asteroids) {
+		if (a.x < -32) a.x = width + 32;
+		if (a.x > width + 32) a.x = -32;
+		if (a.y < -32) a.y = height + 32;
+		if (a.y > height + 32) a.y = -32;
+    a.addSpeed(0.01);
+	}
+
+  if (kitty.collides(asteroids)) {
+    endGame();
+  }
+}
+
+function createAsteroids() {
+  for (let i = 0; i < 10; i++) {
+    let a = new asteroids.Sprite(random(width), height, 64);
+    a.speed = 2;
+    a.mass = 2
+    a.rotationSpeed = random(-1, 1);
+    a.direction = random(360);
+  }
+}
+
+function endGame() {
+  asteroids.remove();
+  survivalTime = 0;
+  tutorialOver = true;
+  createAsteroids();
+}
+
+function timer() {
+  survivalTime += 1;
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
 }
